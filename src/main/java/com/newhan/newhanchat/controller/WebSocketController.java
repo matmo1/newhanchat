f2b20ff1dee1f6deec187959fcea3623959e7867
matmo1 <martin060607@gmail.com>
2025-04-20 16:50:31 +0300

    Added websocket

---
+++ src/main/java/com/newhan/newhanchat/controller/WebSocketController.java
@@ -0,0 +1,36 @@
+package com.newhan.newhanchat.controller;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.bson.types.ObjectId;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+
+import com.newhan.newhanchat.dto.ChatMessageDTO;
+import com.newhan.newhanchat.dto.SendMessageDTO;
+import com.newhan.newhanchat.service.ChatMessageService;
+
+@Controller
+public class WebSocketController {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatMessageService chatMessageService;
+
+    public WebSocketController(SimpMessagingTemplate messagingTemplate, ChatMessageService chatMessageService) {
+        this.messagingTemplate = messagingTemplate;
+        this.chatMessageService = chatMessageService;
+    }
+
+    @MessageMapping("/chat")
+    public void processMessage(@Payload SendMessageDTO messageDTO, @Header("simpSeesioId") ObjectId sessionId, @AuthenticationPrincipal ObjectId  senderId) {
+        ChatMessageDTO savedMessage = chatMessageService.saveMessage(messageDTO, senderId);
+
+        messagingTemplate.convertAndSendToUser(messageDTO.recipientId().toString(), "/queue/messages", savedMessage);
+
+        messagingTemplate.convertAndSendToUser(senderId.toString(), "/queue/notifications", Map.of("type", "DELIVERY_CONFIRMATION", "messageId", savedMessage.id(), "timestamp", LocalDateTime.now()));
+    }
+}
