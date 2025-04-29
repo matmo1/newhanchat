518d39c67b35c210c0142d3eb777e077f663abde
matmo1 <martin060607@gmail.com>
2025-04-29 12:45:22 +0300

    Added security config

--- src/main/java/com/newhan/newhanchat/config/websocket/WebSocketSecurityConfig.java
+++ src/main/java/com/newhan/newhanchat/config/websocket/WebSocketSecurityConfig.java
@@ -2,15 +2,27 @@ package com.newhan.newhanchat.config.websocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
+
+import static org.springframework.messaging.simp.SimpMessageType.*;
 
 @Configuration
 @EnableWebSocketSecurity
 public class WebSocketSecurityConfig {
+
     @Bean
     MessageMatcherDelegatingAuthorizationManager.Builder messageAuthorizationManager() {
+        return MessageMatcherDelegatingAuthorizationManager.builder()
+            .simpDestMatchers("/app/**").authenticated()
+            .simpSubscribeDestMatchers("/user/**").authenticated()
+            .simpTypeMatchers(CONNECT, UNSUBSCRIBE, DISCONNECT).permitAll()
+            .anyMessage().denyAll();
+    }
 
+    @Bean
+    SecurityContextChannelInterceptor securityContextChannelInterceptor() {
+        return new SecurityContextChannelInterceptor();
     }
 }
